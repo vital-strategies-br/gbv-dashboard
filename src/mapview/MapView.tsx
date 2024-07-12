342743e8c472f8f698307b079bc0f2a1fd053805
arthurlorenzi <lorenzi.arthur@gmail.com>
2024-07-12 14:48:31 -0300

    updates to strcuture

--- src/mapview/MapView.tsx
+++ src/mapview/MapView.tsx
@@ -1,32 +1,86 @@
-import React from 'react';
+import React, { useState } from "react";
+import Select from "react-select";
 
-import SVGMap from './SVGMap';
-import SectionHeader from '../common/SectionHeader';
+import SVGMap from "./SVGMap";
+import SectionHeader from "../common/SectionHeader";
 
-import './MapView.css';
+import data from "./data/dashboard.json";
+import Demographic from "../icons/demographic.svg";
 
-function Dashboard() {
+import "./MapView.css";
+
+function MapView() {
+  let [selectedNeighborhood, setSelectedNeighborhood] = useState<number | null>(
+    null
+  );
+
+  function onShapeClick(id: number) {
+    if (id === selectedNeighborhood) {
+      setSelectedNeighborhood(null);
+    } else {
+      setSelectedNeighborhood(id);
+    }
+  }
+
+  let filtered = Object.fromEntries(
+    data.map((neighborhood) => {
+      return [
+        neighborhood.id_shape,
+        {
+          name: neighborhood.name,
+          value: neighborhood.data[4].subnotification_rate,
+        },
+      ];
+    })
+  );
+
+  let name = data.find(
+    (neighborhood) => neighborhood.id_shape === selectedNeighborhood
+  )?.name;
+
+  console.log(name);
+
   return (
-		<div className="dashboard-viewport">
-			<SectionHeader />
-			<div className="dashboard-content">
-				<div className="dashboard-block">
-					<div></div>
-					<div>
-						<SVGMap onShapeClick={alert} colors={{ 3:'blue', 25: 'orange', 71: 'red' }} />
-					</div>
-				</div>
-				<div className="dashboard-block">
-					O mapa do Recife à esquerda mostra a estimativa de subnotificação de casos de 
-					violência contra a mulher para cada 10.000 usuárias de atenção básica (AB) em
-					cada bairro. O gráfico de barras agrupa bairros em faixa de estimativa similares.
-					Ao passar o mouse sobre cada barra, destacam-se os bairros com quantidades similares
-					de possíveis casos de violência não identficados pelo sistema de saúde para aquelas
-					localidades.
-				</div>
-			</div>
-		</div>
+    <div className="mapview-container">
+      <SectionHeader
+        title="Subnotificação de violência contra mulher no município de Recife"
+        subtitle="Estimativa de Subnotificação por 10.000 usuárias das atenção básica."
+        iconSrc={Demographic}
+        iconAlt="Icon of a group of people"
+      />
+      <div className="dashboard-content">
+        <div className="dashboard-content-inner content">
+          <div className="dashboard-block">
+            <div>
+              <SVGMap
+                selectedShape={selectedNeighborhood}
+                onShapeClick={onShapeClick}
+                data={filtered}
+              />
+            </div>
+          </div>
+          <div className="dashboard-block">
+            {selectedNeighborhood ? (
+              <h3>{name}</h3>
+            ) : (
+              <>
+                <p>
+                  O mapa do Recife à esquerda mostra a estimativa de
+                  subnotificação de casos de violência contra a mulher para cada
+                  10.000 usuárias de atenção básica (AB) em cada bairro. O
+                  gráfico de barras agrupa bairros em faixa de estimativa
+                  similares. Ao passar o mouse sobre cada barra, destacam-se os
+                  bairros com quantidades similares de possíveis casos de
+                  violência não identficados pelo sistema de saúde para aquelas
+                  localidades
+                </p>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
-export default Dashboard;
+export default MapView;
--- src/mapview/SVGMap.tsx
+++ src/mapview/SVGMap.tsx
@@ -3,16 +3,48 @@ import React from 'react';
 import './SVGMap.css';
 import shapes from './shapes/recife-4098pts.json';
 
-interface MapProps {
-  // The 'id' of the selected shape
-  selectedShape?: number,
+const PALETTE = [
+  "#FFF0F0",
+  "#FFE1E2",
+  "#FFC7C8",
+  "#FFA0A2",
+  "#FF5054",
+  "#F53D40",
+  "#E61E21",
+  "#B62023",
+  "#931F21",
+  "#7E2021",
+  "#480708"
+];
+const NULL_COLOR = "#d4d4d4";
+
+interface ShapeData {
+  name: string,
+  value: number | null,
+}
+
+interface SVGMapProps {
   // A mapping of shape 'id' to color
-  colors: { [index: number]: string },
+  data: { [index: number]: ShapeData },
+  // The 'id' of the selected shape
+  selectedShape: number | null,
   // The onShapeClick callback
   onShapeClick: (id: number) => void;
 }
 
-function Map({ colors, onShapeClick }: MapProps) {
+function valueToHex(value: number | null, range: number = 1): string {
+  if (value === null) return NULL_COLOR;
+  if (value >= range) return PALETTE[PALETTE.length - 1];
+  if (value <= 0) return PALETTE[0];
+
+  const paletteSize = PALETTE.length - 1; // Subtracting 1 because the last color is reserved for 'range'
+  const stepSize = range / paletteSize;
+  const index = Math.min(Math.floor(value / stepSize), paletteSize);
+
+  return PALETTE[index];
+}
+
+function SVGMap({ data, selectedShape, onShapeClick }: SVGMapProps) {
 
   function onPathClick(id: number): void {
     if (onShapeClick) {
@@ -32,8 +64,10 @@ function Map({ colors, onShapeClick }: MapProps) {
         stroke-linejoin="round"
       >
         {shapes.locations.map(x => {
+          let shapeData = data[x.id];
+          let fill = (!selectedShape || selectedShape === x.id) ? valueToHex(shapeData.value) : NULL_COLOR
           return (
-            <path d={x.path} onClick={() => onPathClick(x.id)} fill={colors[x.id] || "#d4d4d4"}></path>
+            <path d={x.path} onClick={() => onPathClick(x.id)} stroke="white" fill={fill}></path>
           );
         })}
       </svg>
@@ -41,4 +75,4 @@ function Map({ colors, onShapeClick }: MapProps) {
   );
 }
 
-export default Map;
+export default SVGMap;
