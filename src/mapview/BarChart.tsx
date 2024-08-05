32f24a574fc6ad98056f4dbb5cfbd35a87854623
arthurlorenzi <lorenzi.arthur@gmail.com>
2024-08-05 20:49:09 -0300

    Update ticks in bar chart and include n.d. (n/a) count

--- src/mapview/BarChart.tsx
+++ src/mapview/BarChart.tsx
@@ -5,128 +5,153 @@ import "./BarChart.css";
 
 const MAX_VALUE = 1;
 const BINS = 25;
-const YLIM = [0, 30];
+const YLIM: [number, number] = [0, 30];
 
 function linspace(
-  start: number,
-  end: number,
-  num: number,
-  roundSteps: boolean = false
+    start: number,
+    end: number,
+    num: number,
+    roundSteps: boolean = false
 ): number[] {
-  const result = [];
-  const totalRange = end - start;
-  let step = totalRange / (num - 1);
-  if (roundSteps) {
-    step = Math.round(step);
-  }
-  for (let i = 0; i < num; i++) {
-    result.push(start + step * i);
-  }
-  return result;
+    const result = [];
+    const totalRange = end - start;
+    let step = totalRange / (num - 1);
+    if (roundSteps) {
+        step = Math.round(step);
+    }
+    for (let i = 0; i < num; i++) {
+        result.push(start + step * i);
+    }
+    return result;
 }
 
 interface BarProps {
-  value: number;
-  maxDataValue: number;
-  width: number;
-  plotHeight: number;
-  index: number;
-  totalBars: number;
-  yAxisLimits: Array<number>;
+    value: number;
+    maxDataValue: number;
+    width: number;
+    plotHeight: number;
+    index: number;
+    totalBars: number;
+    yAxisLimits: Array<number>;
 }
 
 function Bar({
-  value,
-  maxDataValue,
-  width,
-  plotHeight,
-  index,
-  totalBars,
-  yAxisLimits,
+    value,
+    maxDataValue,
+    width,
+    plotHeight,
+    index,
+    totalBars,
+    yAxisLimits,
 }: BarProps) {
-  const barHeight = value * (plotHeight / yAxisLimits[1]); // Adjust height for padding
-  const barWidth = width / totalBars; // Adjust width for padding between bars
+    const barHeight = value * (plotHeight / yAxisLimits[1]); // Adjust height for padding
+    const barWidth = width / totalBars; // Adjust width for padding between bars
 
-  const getColor = (value: number) => {
-    if (value > 30) return "darkred";
-    if (value > 20) return "red";
-    if (value > 10) return "lightcoral";
-    return "gray";
-  };
+    const getColor = (value: number) => {
+        if (value > 30) return "darkred";
+        if (value > 20) return "red";
+        if (value > 10) return "lightcoral";
+        return "gray";
+    };
 
-  return (
-    <g
-      transform={`translate(${index * (width / totalBars)}, ${
-        plotHeight - barHeight
-      })`}
-    >
-      <rect
-        x="0"
-        y="0"
-        width={barWidth}
-        height={barHeight}
-        fill={getColor(value)}
-      />
-      {/* <text
-          x={barWidth / 2}
-          y={barHeight + 15}
-          textAnchor="middle"
-          fontSize="10"
-          fill="black"
-        >
-          {value}
-        </text> */}
-    </g>
-  );
+    return (
+        <rect
+            x="0"
+            y="0"
+            width={barWidth}
+            height={barHeight}
+            fill={getColor(value)}
+        />
+    );
 }
 
 interface AxisGridTicksProps {
     axis: 'x' | 'y';
     limits: [number, number];
-    numTicks: number;
-    chartAreaLength: number;
-    chartAreaOther: number;
-    chartMargin: number;
-  }
+    numTicks?: number;
+    axisLength: number;
+    otherLength: number;
+    startMargin: number;
+    axisOffset?: number,
+    showLines?: boolean;
+    roundSteps?: boolean;
+}
 
-// function AxisGridTicks({axis, limits, numTicks = 5, chartAreaLength, chartAreaOther, chartMargin}: AxisGridTicksProps)  {
-//     return (
-//         <g className={`${axis}-grid-ticks`}>
-//             {linspace(limits[0], limits[1], numTicks).map((value) => {
-//                 const percentage = 0;
-//             })}
-//         </g>
-//     )
-// }
+function AxisGridTicks({ axis, limits, numTicks = 5, axisLength, otherLength, startMargin, axisOffset = 0, showLines = false, roundSteps = false }: AxisGridTicksProps) {
+    const tickPositions = linspace(limits[0], .9 * limits[1], numTicks, roundSteps);
+
+    return (
+        <g className={`${axis}-grid-ticks`}>
+            {tickPositions.map((value) => {
+                const lengthPercentage = (value - limits[0]) / limits[1];
+                const lengthInPixels = (lengthPercentage * (axisLength - axisOffset)) + axisOffset;
+                let x1: number, y1: number, x2: number, y2: number, textOffsetX: number, textOffsetY: number, anchor: string;
+
+                if (axis === "x") {
+                    x1 = lengthInPixels + startMargin;
+                    x2 = x1;
+                    y1 = otherLength;
+                    y2 = 0;
+                    textOffsetX = 0;
+                    textOffsetY = 30;
+                    anchor = "middle";
+                } else {
+                    x1 = startMargin;
+                    x2 = startMargin + otherLength;
+                    y1 = axisLength - lengthInPixels;
+                    y2 = y1;
+                    textOffsetX = -10;
+                    textOffsetY = 0;
+                    anchor = "end";
+                }
+
+                return (
+                    <g key={value}>
+                        {showLines && <line
+                            x1={x1}
+                            y1={y1}
+                            x2={x2}
+                            y2={y2}
+                        />}
+                        <text x={x1 + textOffsetX} y={y1 + textOffsetY} textAnchor={anchor}>
+                            {roundSteps ? value : value.toFixed(2)}
+                        </text>
+                    </g>
+                );
+            })}
+        </g>
+    )
+}
 
 function BarChart({ data, width = 600, height = 350 }: BarChartProps) {
-  const values = Object.values(data).map((x) => x.subnotification_rate);
+    const values = Object.values(data).map((x) => x.subnotification_rate);
 
-  let nullCount = 0;
-  const binSize = MAX_VALUE / BINS;
-  const binCounts = new Array(BINS).fill(0);
+    let nullCount = 0;
+    const binSize = MAX_VALUE / BINS;
+    const binCounts = new Array(BINS).fill(0);
 
-  for (const value of values) {
-    if (value === null) nullCount++;
-    else binCounts[Math.min(Math.floor(value / binSize), BINS - 1)]++;
-  }
+    for (const value of values) {
+        if (value === null) nullCount++;
+        else binCounts[Math.min(Math.floor(value / binSize), BINS - 1)]++;
+    }
 
-  const chartMarginLeft = 70;
-  const chartMarginBottom = 70;
+    const chartMarginLeft = 70;
+    const chartMarginBottom = 70;
 
-  const chartAreaWidth = width - chartMarginLeft;
-  const chartAreaHeight = height - chartMarginBottom;
+    // We subtract 30 to have some padding for tick labels
+    const chartAreaWidth = width - chartMarginLeft;
+    const chartAreaHeight = height - chartMarginBottom;
 
-  console.log(binCounts);
+    const naBarHeight = nullCount * ((chartAreaHeight) / YLIM[1]);
 
-  return (
-    <svg
-      width="100%"
-      height={350}
-      viewBox={`0 0 ${width} ${height}`}
-      preserveAspectRatio="xMidYMin"
-    >
-      {/* <g>
+    return (
+        <svg
+            width="100%"
+            height={350}
+            viewBox={`0 0 ${width} ${height}`}
+            preserveAspectRatio="xMidYMin"
+        >
+            {/* <g>
         {binCounts.map((count, index) => (
           <Bar
             key={index}
@@ -140,60 +165,29 @@ function BarChart({ data, width = 600, height = 350 }: BarChartProps) {
           />
         ))}
       </g> */}
-      <g className="y-grid-ticks">
-        {linspace(YLIM[0], YLIM[1], 5, true).map((value) => {
-          const heightAsPercentage = value / YLIM[1];
-          // We subtract 30 pixels from the height to have room for the grid labels
-          const heightInPixels = heightAsPercentage * (chartAreaHeight - 30);
-          const y = chartAreaHeight - heightInPixels;
-
-          return (
-            <g key={value}>
-              <line
-                x1={chartMarginLeft}
-                y1={y}
-                x2={width}
-                y2={y}
-              />
-              <text x={chartMarginLeft-10} y={y}>
-                {value}
-              </text>
+            <AxisGridTicks axis="x" limits={[0, MAX_VALUE]} axisLength={chartAreaWidth} otherLength={chartAreaHeight} startMargin={chartMarginBottom} axisOffset={16 + 32 + 48} />
+            <AxisGridTicks axis="y" limits={YLIM} axisLength={chartAreaHeight} otherLength={chartAreaWidth} startMargin={chartMarginLeft} showLines roundSteps />
+            <g className="barchart-bar-na">
+                <rect x={16 + chartMarginLeft} y={chartAreaHeight - naBarHeight} width="32" height={naBarHeight} />
+                <text x={16 + chartMarginLeft + 16} y={chartAreaHeight + 30} textAnchor="middle">n.d.</text>
             </g>
-          );
-        })}
-      </g>
-      <g className="x-grid-ticks">
-        {linspace(0, MAX_VALUE, 5).map((value) => {
-          const widthAsPercentage = value / MAX_VALUE;
-          const widthtInPixels = widthAsPercentage * (chartAreaWidth - 30);
-          const x = chartMarginLeft + widthtInPixels;
-
-          return (
-            <text x={x + 10} y={chartAreaHeight + 30} textAnchor="start">
-              {value}
+            <text
+                x={chartAreaWidth / 2 + chartMarginLeft}
+                y={height - 4}
+                className="barchart-label"
+            >
+                Número estimado de casos subnotificados por 10 mil usuárias AB
             </text>
-          );
-        })}
-      </g>
-      <text
-        x={chartAreaWidth / 2 + chartMarginLeft}
-        y={height - 4}
-        textAnchor="middle"
-        fontSize="14"
-      >
-        Número estimado de casos subnotificados por 10 mil usuárias AB
-      </text>
-      <text
-        x={-height / 2}
-        y={16}
-        textAnchor="middle"
-        fontSize="14"
-        transform="rotate(-90)"
-      >
-        Número de Bairros
-      </text>
-    </svg>
-  );
+            <text
+                x={-height / 2}
+                y={16}
+                transform="rotate(-90)"
+                className="barchart-label"
+            >
+                Número de Bairros
+            </text>
+        </svg>
+    );
 }
 
 export default BarChart;
